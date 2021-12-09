1616fe8c096505f1c27d5482891d86c62317d5a5
bribrie <ibriebriee@gmail.com>
2021-12-09 18:52:48 +0900

    Drawing a face

--- tutorial/script.js
+++ tutorial/script.js
@@ -1,50 +1,30 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-// //* fillRect()
-// ctx.fillStyle = "green";
-// ctx.fillRect(20, 20, 150, 100);
-// ctx.fillStyle = "blue";
-// ctx.fillRect(200, 20, 150, 100);
-
-// //* strokeRect()
-// ctx.lineWidth = 5;
-// ctx.strokeStyle = "green";
-// ctx.strokeRect(100, 200, 150, 100);
-
-// //* clearRect()
-// ctx.clearRect(25, 25, 140, 90);
-
-// //* fillText()
-// ctx.font = "30px Arial";
-// ctx.fillStyle = "purple";
-// ctx.fillText("Hello world!", 400, 50);
-
-// //* strokeText()
-// ctx.lineWidth = 1;
-// ctx.strokeStyle = "orange";
-// ctx.strokeText("Hello world!", 400, 100);
-
-//* Paths
-//Triangle
+//Arc (circles)
 ctx.beginPath();
-ctx.moveTo(50, 50); //시작점
-ctx.lineTo(150, 50);
-ctx.lineTo(100, 200);
-ctx.lineTo(50, 50);
-ctx.fillStyle = "bisque";
-ctx.fill();
+const centerX = canvas.width / 2;
+const centerY = canvas.height / 2;
 
-ctx.beginPath();
-ctx.moveTo(200, 50);
-ctx.lineTo(150, 200);
-ctx.lineTo(250, 200);
-ctx.closePath();
-ctx.strokeStyle = "coral";
+//* Draw head
+//arc(x, y, radius, startAngle, endAngle, anticlockwise);
+ctx.arc(centerX, centerY, 200, 0, Math.PI * 2);
+
+//* Move to mouth
+ctx.moveTo(centerX + 100, centerY);
+
+//* Draw mouth
+ctx.arc(centerX, centerY, 100, 0, Math.PI, false);
+
+//* Move left eye
+ctx.moveTo(centerX - 60, centerY - 80);
+
+//* Draw left eye
+ctx.arc(centerX - 80, centerY - 80, 20, 0, Math.PI * 2);
+
+//* Move right eye
+ctx.moveTo(centerX + 100, centerY - 80);
+
+//* Draw right eye
+ctx.arc(centerX + 80, centerY - 80, 20, 0, Math.PI * 2);
 ctx.stroke();
-
-//Rectangle
-ctx.beginPath();
-ctx.rect(300, 50, 150, 100);
-ctx.fillStyle = "teal";
-ctx.fill();
